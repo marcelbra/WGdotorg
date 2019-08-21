cb87c3c3da1bca720cea7034b5b5fbd7be4b6d8e
marcelbra <marcelbraasch@gmail.com>
2019-08-21 11:13:09 +0200

    Initial Commit

---
+++ WGdotorg/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WGdotorg
+//
+//  Created by Marcel Braasch on 21.08.19.
+//  Copyright © 2019 Marcel Braasch. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WGdotorg/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WGdotorg
+//
+//  Created by Marcel Braasch on 21.08.19.
+//  Copyright © 2019 Marcel Braasch. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
