244b9585cbed9206b542f5f886a3208c2b1d1b44
marcelbra <marcelbraasch@gmail.com>
2019-08-26 00:39:28 +0200

    Save files

--- WGdotorg/CreateGroupViewController.swift
+++ WGdotorg/CreateGroupViewController.swift
@@ -13,38 +13,52 @@ class CreateGroupViewController: UIViewController {
 
     
     @IBOutlet weak var groupNameTextField: UITextField!
-    @IBOutlet weak var errorMessage: UITextField!
     
     static var db = Firestore.firestore()
     
     // Create user friendly UUID and check in database if it already exists
     func createUuid() -> String {
-        var again = false
-        var uuid = UUID().uuidString
-        uuid = uuid.components(separatedBy: "-")[0]
-        CreateGroupViewController.db.collection("groups").getDocuments() { (querySnapshots, err) in
-            if let _ = err {
-//                self.errorMessage.text = "An error occured."
+//        var again = false
+//        var uuid = UUID().uuidString
+//        uuid = uuid.components(separatedBy: "-")[0]
+//        uuid = "test"
+        var test = [QueryDocumentSnapshot]()
+        CreateGroupViewController.db.collection("groups").getDocuments() {
+            (querySnapshot, err) in
+            if let err = err {
+                print("Error getting documents: \(err)")
             } else {
-                // Check every document if uid already exisits
-                for doc in querySnapshots!.documents {
-                    // Get the uid from current group and cast it to string
-                    let docUid = doc.data()["uid"] as! String
-                    // If uids are equal start over
-                    if uuid == docUid {
-                        again = true
-                    }
+                for document in querySnapshot!.documents {
+                    test += [document]
                 }
             }
         }
-        
-        if again {
-            return uuid
-        }
-        else {
-            return createUuid()
-        }
+        return ""
     }
+        
+//        CreateGroupViewController.db.collection("groups").getDocuments() { (querySnapshots, err) in
+//            if let _ = err {
+////                self.errorMessage.text = "An error occured."
+//            } else {
+//                // Check every document if uid already exisits
+//                for doc in querySnapshots!.documents {
+//                    // Get the uid from current group and cast it to string
+//                    let docUid = doc.data()["uid"] as! String
+//                    // If uids are equal start over
+//                    if uuid == docUid {
+//                        again = true
+//                    }
+//                }
+//            }
+//        }
+    
+//        if again {
+//            return uuid
+//        }
+//        else {
+//            return createUuid()
+//        }
+//    }
 
     @IBAction func didTapCreateGroup(_ sender: UIButton) {
         
